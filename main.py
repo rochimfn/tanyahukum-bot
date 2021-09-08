8e1d2ac733d014326ba90090935029f7ca872024
Rochim Farul Noviyan <rochim.noviyan@gmail.com>
2021-09-08 10:26:21 +0700

    change to webhook mode

--- main.py
+++ main.py
@@ -5,32 +5,56 @@ import logging
 import os
 import sys
 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 def setup():
     load_dotenv()
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
-    return { 'token': os.environ['TOKEN'] }
+    try:
+        config = { 'token': os.environ['TOKEN'], 'port': int(os.environ.get('PORT', 5000)) }
+        logger.info('Token ditemukan!')
+        return config
+    except KeyError:
+        logger.warning('Token tidak ditemukan!')
+        sys.exit()
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text='Selamat datang di Tanyahukum(dev)!')
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Silahkan bertanya :)')
+
+def help(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Untuk mencari, kirim kata kunci atau pertanyaan')
 
 def answer(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=ask(update.message.text))
 
+def error(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
 
 def main():
-    TOKEN = setup()['token']
-    if TOKEN is None:
-        sys.exit('token not provided')
+    config = setup()
+    TOKEN = config['token']
+    PORT = config['port']
+
     updater = Updater(token=TOKEN, use_context=True)
     start_handler = CommandHandler('start', start)
+    help_handler = CommandHandler('help', help)
     answer_handler = MessageHandler(Filters.text & (~Filters.command), answer)
 
     dispatcher = updater.dispatcher
     dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(help_handler)
     dispatcher.add_handler(answer_handler)
-    updater.start_polling()
+
+    dispatcher.add_error_handler(error)
+
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://tanyahukumbotsrv.herokuapp.com/' + TOKEN)
+    
+    updater.idle()
 
 if __name__ == '__main__':
     main()
